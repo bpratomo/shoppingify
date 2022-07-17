9c7c7e34f9d95ff64f051ff2d3037f4063792589
budi <buddy.pratomo@gmail.com>
2022-07-17 21:21:37 +0200

    cleanup part 2

--- src/pages/ItemDesc.tsx
+++ src/pages/ItemDesc.tsx
@@ -45,7 +45,7 @@ export const ItemDesc: FC<ItemDescProps> = (props) => {
         <i className="fas fa-long-arrow-alt-left"> </i> back
       </div>
       <div className={styles.imageContainer}>
-        <img src={props.item.imageUrl} alt="Item's image" />
+        <img src={props.item.imageUrl} alt="" />
       </div>
       <div className={styles.text}>
         <div className={styles.label}>name</div>
